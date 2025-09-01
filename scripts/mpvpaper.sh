d7aa1c90d485cf8e5fd949f6aa3cd17c13f1ae4b
Christian Kern <christian.kern@me.com>
2025-09-02 00:56:59 +0200

    bugfixes

--- scripts/mpvpaper.sh
+++ scripts/mpvpaper.sh
@@ -2,4 +2,4 @@
 
 sleep 2
 mpvpaper -o "--loop --no-audio" DP-3 "$HOME/Bilder/animated backgrounds/draeonor.mp4"
-# $HOME/.local/bin/matugen image /home/khrom/Bilder/Wallpapers/draeonor.png -m "dark"
+
